1015f31eb2a0e47b2f113c093143a6b153e322e2
gowdhambarati <32629550+gowdhambarati@users.noreply.github.com>
2017-10-09 11:31:10 +0530

    Add files via upload

---
+++ kavi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("\n enter the number");
+    scanf("%d",&a);
+    b=a%2;
+    if(b==0)
+{
+printf("number is even",&b);
+}
+else
+{
+    printf("number is odd",&b);
+}
+getch();
+}
